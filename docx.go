75ae40986f1b30bc5084386ac57ca3469c3949fc
Habib Ridho <habibridho@live.com>
2020-05-01 20:08:35 +0700

    Add New method to create docx object

--- docx.go
+++ docx.go
@@ -1,7 +1,9 @@
 package cetak
 
 import (
+	"archive/zip"
 	"bytes"
+	"io"
 	"text/template"
 )
 
@@ -14,6 +16,40 @@ type Docx interface {
 type docx struct {
 	tpl    *template.Template
 	buffer bytes.Buffer
+}
+
+// New creates a new Docx object. It will return error if template file could not be opened.
+func New(templatePath string) (Docx, error) {
+	reader, err := zip.OpenReader(templatePath)
+	if err != nil {
+		return nil, err
+	}
+	defer reader.Close()
+
+	var tplBuffer bytes.Buffer
+	for _, f := range reader.File {
+		if f.Name == "word/document.xml" {
+			contentReader, err := f.Open()
+			if err != nil {
+				return nil, err
+			}
+			defer contentReader.Close()
+
+			_, err = io.Copy(&tplBuffer, contentReader)
+			if err != nil {
+				return nil, err
+			}
+		}
+	}
+
+	tpl, err := template.New("template").Parse(tplBuffer.String())
+	if err != nil {
+		return nil, err
+	}
+
+	return &docx{
+		tpl: tpl,
+	}, nil
 }
 
 func (d *docx) Generate(data interface{}) error {
