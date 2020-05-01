b2823af8430c2748155e9c810841fd6842c64c2a
Habib Ridho <habibridho@live.com>
2020-05-01 17:32:14 +0700

    Implement Generate method

--- docx.go
+++ docx.go
@@ -1,14 +1,22 @@
 package cetak
 
-import "errors"
+import (
+	"bytes"
+	"text/template"
+)
 
 // Docx is the interface used to interact with cetak's doc generator
 type Docx interface {
-	Generate(data map[string]interface{}) error
+	// Generate the document using given data. This will write the result to internal buffer.
+	Generate(data interface{}) error
 }
 
-type docx struct{}
+type docx struct {
+	tpl    *template.Template
+	buffer bytes.Buffer
+}
 
-func (d *docx) Generate(data map[string]interface{}) error {
-	return errors.New("no implementation")
+func (d *docx) Generate(data interface{}) error {
+	err := d.tpl.Execute(&d.buffer, data)
+	return err
 }
--- docx_test.go
+++ docx_test.go
@@ -2,12 +2,31 @@ package cetak
 
 import (
 	"testing"
+	"text/template"
 
 	"github.com/stretchr/testify/assert"
 )
 
+type TestData struct {
+	TestContent string
+}
+
 func TestGenerate(t *testing.T) {
-	d := &docx{}
-	err := d.Generate(nil)
+	mockTemplate, err := template.New("template").Parse("Hello {{.TestContent}}")
+	if err != nil {
+		t.Fatalf("could not parse mock tempalte")
+	}
+	d := &docx{
+		tpl: mockTemplate,
+	}
+	data := TestData{
+		TestContent: "World!",
+	}
+
+	err = d.Generate(data)
 	assert.NoError(t, err)
+
+	expected := "Hello World!"
+	actual := d.buffer.String()
+	assert.Equal(t, expected, actual)
 }
