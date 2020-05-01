749dc831716876e7555ff802c191690c5698f848
Habib Ridho <habibridho@live.com>
2020-05-02 06:23:16 +0700

    Add test with advanced template

--- docx_test.go
+++ docx_test.go
@@ -2,6 +2,7 @@ package cetak
 
 import (
 	"fmt"
+	"log"
 	"os"
 	"testing"
 
@@ -10,37 +11,66 @@ import (
 )
 
 type TestData struct {
-	Title   string
-	Content string
+	Title             string
+	Content           string
+	List              []string
+	UndefinedPipeline string
+}
+
+type TestCase struct {
+	name         string
+	templatePath string
+	expectedPath string
+	data         TestData
 }
 
 func TestGenerate(t *testing.T) {
-	d, err := New("sample_templates/simple-template.docx")
-	if err != nil {
-		t.Fatalf("could not create docx object: %s", err.Error())
+	cases := []TestCase{
+		{
+			name:         "test with simple template",
+			templatePath: "sample_templates/simple-template.docx",
+			expectedPath: "test_resources/simple-result.docx",
+			data: TestData{
+				Title:   "Some Title",
+				Content: "Some Content",
+			},
+		},
+		{
+			name:         "test with advanced template",
+			templatePath: "sample_templates/advanced-template.docx",
+			expectedPath: "test_resources/advanced-result.docx",
+			data: TestData{
+				Title:   "Some Title",
+				Content: "Some Content",
+				List:    []string{"Apple", "Banana", "Cherry", "Durian"},
+			},
+		},
 	}
 
-	data := TestData{
-		Title:   "Some Title",
-		Content: "Some Content",
-	}
+	for _, c := range cases {
+		log.Println(c.name)
+		d, err := New(c.templatePath)
+		if err != nil {
+			t.Fatalf("could not create docx object: %s", err.Error())
+		}
 
-	id, err := uuid.NewRandom()
-	if err != nil {
-		t.Fatalf("could not generate uuid: %s", err.Error())
-	}
-	destination := fmt.Sprintf("%s.docx", id.String())
-	err = d.Generate(data, destination)
-	assert.NoError(t, err)
-	defer os.Remove(destination)
+		id, err := uuid.NewRandom()
+		if err != nil {
+			t.Fatalf("could not generate uuid: %s", err.Error())
+		}
+		destination := fmt.Sprintf("%s.docx", id.String())
+		err = d.Generate(c.data, destination)
+		assert.NoError(t, err)
+		defer os.Remove(destination)
 
-	expected, err := getDocxContentAsString("test_resources/simple-result.docx")
-	if err != nil {
-		t.Fatalf("could not read test resource file: %s", err.Error())
+		expected, err := getDocxContentAsString(c.expectedPath)
+		if err != nil {
+			t.Fatalf("could not read test resource file: %s", err.Error())
+		}
+		actual, err := getDocxContentAsString(destination)
+		if err != nil {
+			t.Fatalf("could not read test resource file: %s", err.Error())
+		}
+		assert.Equal(t, expected, actual)
 	}
-	actual, err := getDocxContentAsString(destination)
-	if err != nil {
-		t.Fatalf("could not read test resource file: %s", err.Error())
-	}
-	assert.Equal(t, expected, actual)
 }
